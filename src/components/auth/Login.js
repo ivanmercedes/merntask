0f29b01424d090b75253e1535d6fb59976c82a1b
Ivan Mercedes <ivanmercede@gmail.com>
2020-07-29 22:22:59 +0200

    archivos requeridos

---
+++ src/components/auth/Login.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Login = () => {
+
+    const onChange =() =>{
+
+    }
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesion</h1>
+
+                <fom>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Tu Email"
+                        onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Tu Password"
+                        onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                        type="submit"
+                         className="btn btn-primario btn-block"
+                         value="Iniciar Sesion"
+                        />
+                    </div>
+                </fom>
+            </div>
+        </div>
+     );
+}
+ 
+export default Login;
